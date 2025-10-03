311a7adc9e940e60be1d3c72d9b4c438a2de97be
MaxS2027 <max.schwartz2027@gbwl.org>
2025-10-03 12:43:15 +0000

    1.8 notes

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
+/** 
+ * @MaxS2027
+ * @since oct 3 2025
+ * @version 1.0
+*/
 public class Main {
-
+   /**
+    * @param input is an array of string
+    * @return VOID means NO OUTPUT
+    */
    public static void main(String []args) {
       System.out.println("Hello World");
    }
